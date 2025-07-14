7828db2a8777b85725758011572d27f34d16df8a
Alexander Serebryakov <34211186+alexander-serebryakov@users.noreply.github.com>
2025-07-14 21:58:16 +0100

    Add files via upload

--- snake/main.cpp
+++ snake/main.cpp
@@ -1,7 +1,7 @@
-#include <SFML/Graphics.hpp>
+#include "raylib.h"
 #include <time.h>
 #include <iostream>
-using namespace sf;
+#include <iomanip>
 
 enum Direction { Down, Left, Right, Up };
 
@@ -50,9 +50,9 @@ void Tick(std::vector<SnakeScale>& snake, Fruit& f, Direction dir, int N, int M,
         // Increment the snake length
         num++;
 
-        // Make a new fruit
-        f.x = arc4random() % N; // new x-coordinate of the fruit
-        f.y = arc4random() % M; // new y-coordinate of the fruit
+        // Make a new frui
+        f.x = GetRandomValue(0, N - 1); // new x-coordinate of the fruit
+        f.y = GetRandomValue(0, M - 1); // new y-coordinate of the fruit
     }
 
     // Check if the snake bites itself
@@ -72,161 +72,166 @@ void Tick(std::vector<SnakeScale>& snake, Fruit& f, Direction dir, int N, int M,
     }
 }
 
+
+class Settings {
+    int screenWidth {30};
+    int screenHeight {45};
+    bool speedup {true};
+    int map_size {20};
+    int fps {60};
+
+public:
+    
+    int get_width () const {
+        return screenWidth;
+    }
+    
+    int get_height () const {
+        return screenHeight;
+    }
+    
+    bool get_speedup () const {
+            return speedup;
+    }
+    
+    int get_fps () const {
+        return fps;
+    }
+
+    int get_map_size () const {
+        return map_size;
+    }
+    
+    void display_settings () {
+        std::cout << "#############################################################################################################\n";
+        std::cout << std::setw(20) << "SETTINGS:\n";
+        std::cout << "SCREEN WIDTH: " << screenWidth << std::endl;
+        std::cout << "SCREEN HEIGHT: " << screenHeight << std::endl;
+        std::cout << "SPEEDUP: " << speedup << std::endl;
+        std::cout << "#############################################################################################################\n";
+    }
+
+    void set_width () {
+        std::cout << "Enter new width:\n";
+        std::cin >> screenWidth;
+    };
+
+    void set_height () {
+        std::cout << "Enter new height:\n";
+        std::cin >> screenHeight;
+    };
+
+    void set_speedup () {
+        std::cout << "Do you want the snake to be speeding up? (1/0)\n";
+        std::cin >> speedup;
+    };
+
+    void set_map_size () {
+        std::cout << "Enter new map size:\n";
+        std::cin >> map_size;
+    };
+    
+};
+
+// 
+
 int main() {
+    SetRandomSeed(time(NULL));
+    SetTraceLogLevel(LOG_NONE); // Suppress all logs
+
+    Settings settings;
+    settings.display_settings();
 
     // Prompt user to use standard game settings
     std:: cout << "Use standard settings? (Y/N)" << std::endl;
     char response;
-    response = tolower(getchar());
-    int N, M;
-    bool speed;
-    if (response == 'y'){
-        N = 20;
-        M = 20;
-        speed = true; 
-    }
-    else {
-        std:: cout << "Enter the window width and height in units." << std::endl;
-        std:: cin >> N >> M;
-        std:: cout << "Would you like the snake to speed up as it grows? (1/0)" << std::endl;
-        std:: cin >> speed;
-    }
-    int size = 20;
+    std::cin >> response;
+    response = std::tolower(response);
 
-    RenderWindow window(VideoMode(size * N, size * (M + 1)), "Snake Game");
+    if (response != 'y') {
+        settings.display_settings();
+        settings.set_width();
+        settings.set_height();
+        settings.set_speedup();
+        settings.set_map_size();
+    }
+
+    // Initialise a window
+    InitWindow(settings.get_width() * settings.get_map_size(), (settings.get_height() + 1) * settings.get_map_size(), "Snake Game");
+
+    // Set frames-per-second
+    SetTargetFPS(settings.get_fps()); 
     
     // Initialise the game variables
-    Clock clock;
     float timer = 0;
     float baseDelay = 0.1; // Base delay
 
     // Initialise the fruit
     Fruit f;
-    f.x = arc4random() % N;
-    f.y = arc4random() % M;
+    f.x = GetRandomValue(0, settings.get_width() - 1);
+    f.y = GetRandomValue(0, settings.get_height() - 1);
 
     // Initialise the snake
     int num = 4; // Length of the snake
     std::vector<SnakeScale> snake; // Vector to store snake segments
     for (int i = 0; i < num; i++) {
-        snake.push_back(SnakeScale(N / 2, M / 2));
+        snake.push_back(SnakeScale(settings.get_width() / 2, settings.get_height() / 2));
     }
     
     // Initial direction of the snake
     Direction dir = Down;
 
-    // Create a render texture for drawing the game board
-    RenderTexture boardTexture;
-    boardTexture.create(size * N, size * M);
+    while (!WindowShouldClose()) {
 
-    // Set up the board cell color
-    Color cellColor;
-    cellColor = Color(200, 200, 200); // Light grey
-
-    // Load the font for displaying the game score
-    Font font;
-    font.loadFromFile("arial.ttf"); 
-    
-    // Create text object for displaying score
-    Text scoreText;
-    scoreText.setFont(font);
-    scoreText.setCharacterSize(16);
-    scoreText.setFillColor(Color::White);
-    scoreText.setPosition(10, M * size);
-
-    while (window.isOpen()) {
-        float time = clock.getElapsedTime().asSeconds();
-        clock.restart();
+        float time = GetFrameTime();
         timer += time;
 
-        Event e;
-        while (window.pollEvent(e)) {
-            if (e.type == Event::Closed) {
-                window.close();
-            }
+        if (WindowShouldClose()) {
+            CloseWindow();
+            return 0;
         }
-        
+
         // Check keyboard input for changing snake direction
-        if (Keyboard::isKeyPressed(Keyboard::Left)) {
-            dir = Left;
-        };
-        if (Keyboard::isKeyPressed(Keyboard::Right)) {
-            dir = Right;
-        };
-        if (Keyboard::isKeyPressed(Keyboard::Up)) {
-            dir = Up;
-        };
-        if (Keyboard::isKeyPressed(Keyboard::Down)) {
-            dir = Down;
-        };
+        if (IsKeyPressed(KEY_LEFT)) dir = Left;
+        if (IsKeyPressed(KEY_RIGHT)) dir = Right;
+        if (IsKeyPressed(KEY_UP)) dir = Up;
+        if (IsKeyPressed(KEY_DOWN)) dir = Down;
 
         // Adjust snake movement delay based on length and speed toggle
-        float delay = speed ? baseDelay - 0.001 * num : baseDelay;
+        float delay = settings.get_speedup() ? baseDelay - 0.001 * num : baseDelay;
 
         // Move the snake based on delay
         if (timer > delay) {
             timer = 0;
-            Tick(snake, f, dir, N, M, num);
+            Tick(snake, f, dir, settings.get_width(), settings.get_height(), num);
         }
-
-        // Update score text
-        scoreText.setString("SCORE: " + std::to_string(num));
-
-        window.clear();
+        
+        BeginDrawing();
 
         // Draw the board
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                // Create a rectangle shape representing the cell
-                RectangleShape cell(Vector2f(size, size));
-                cell.setPosition(i * size, j * size);
-                cell.setFillColor(cellColor);
-
-                // Draw black border around the cell
-                cell.setOutlineThickness(1.0f);
-                cell.setOutlineColor(Color::Black);
-
-                // Draw the cell onto the render texture
-                boardTexture.draw(cell);
+        ClearBackground(BLACK); // Clear the screen
+        for (int i = 0; i < settings.get_width(); i++) {
+            for (int j = 0; j < settings.get_height(); j++) {
+                DrawRectangle(i * settings.get_map_size(), j * settings.get_map_size(), settings.get_map_size(), settings.get_map_size(), GRAY); // Background cells
+                DrawRectangleLines(i * settings.get_map_size(), j * settings.get_map_size(), settings.get_map_size(), settings.get_map_size(), BLACK); // Cell borders
             }
         }
 
         // Draw the snake
         for (const auto& segment : snake) {
-            // Draw a snake segemnt
-            RectangleShape snakeSegment(Vector2f(size, size));
-            snakeSegment.setPosition(segment.x * size, segment.y * size);
-            snakeSegment.setFillColor(Color::Green);
-
-            // Draw black border around the snake segment
-            snakeSegment.setOutlineThickness(1.0f);
-            snakeSegment.setOutlineColor(Color::Black);
-
-            // Draw the segment onto the render texture
-            boardTexture.draw(snakeSegment);
+            DrawRectangle(segment.x * settings.get_map_size(), segment.y * settings.get_map_size(), settings.get_map_size(), settings.get_map_size(), GREEN);
+            DrawRectangleLines(segment.x * settings.get_map_size(), segment.y * settings.get_map_size(), settings.get_map_size(), settings.get_map_size(), BLACK);
         }
-
+        
         // Draw the fruit
-        CircleShape fruitShape(size / 2);
-        fruitShape.setPosition(f.x * size, f.y * size);
-        fruitShape.setFillColor(Color::Red);
-        boardTexture.draw(fruitShape);
-
-        // Display the rendered game board texture
-        boardTexture.display();
-
-        // Copy the rendered texture to a sprite for drawing
-        Texture boardTextureCopy = boardTexture.getTexture();
-        Sprite boardSprite(boardTextureCopy);
-        window.draw(boardSprite);
+        DrawCircle(f.x * settings.get_map_size() + settings.get_map_size() / 2, f.y * settings.get_map_size() + settings.get_map_size() / 2, settings.get_map_size() / 2, RED);
 
         // Draw the score text
-        window.draw(scoreText);
+        DrawText(("SCORE: " + std::to_string(num)).c_str(), 10, settings.get_height() * settings.get_map_size(), 16, WHITE);
 
-        // Update the display
-        window.display();
-
+        EndDrawing();
     }
-
+    CloseWindow();
+    
     return 0;
 }
